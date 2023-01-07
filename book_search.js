b7ac918d5a1fb58f70e928b39d25d6abe893d04e
Kai Elwood-Dieu <kdieu@politico.com>
2023-01-07 13:46:22 -0500

    added test cases

--- book_search.js
+++ book_search.js
@@ -31,7 +31,10 @@
     scannedTextObj.map((book, i) => {
         // map through input book sections
         for (const section of book.Content) {
-            if (section.Text.includes(searchTerm)) {
+
+            // remove commas and punctuation from string
+            let str = section.Text.replace(/,/g, '').replace(/\./g, '');
+            if (str.includes(searchTerm)) {
 
                 // push formatted result to array of results
                 searchResults.Results.push({
@@ -71,6 +74,73 @@ const twentyLeaguesIn = [
         ] 
     }
 ]
+
+const extraBooksIn = [
+    {
+      "Title": "Twenty Thousand Leagues Under the Sea",
+      "ISBN": "9780000528531",
+      "Content": [
+        {
+          "Page": 31,
+          "Line": 8,
+          "Text": "now simply went on by her own momentum.  The dark-"
+        },
+        {
+          "Page": 31,
+          "Line": 9,
+          "Text": "ness was then profound; and however good the Canadian\'s"
+        },
+        {
+          "Page": 31,
+          "Line": 10,
+          "Text": "eyes were, I asked myself how he had managed to see, and"
+        } 
+      ] 
+    },
+    {
+      "Title": "Calvin and Hobbes",
+      "ISBN": "9780000528532",
+      "Content": [
+        {
+          "Page": 1,
+          "Line": 1,
+          "Text": "I'm not dumb. I just have a command of thoroughly useless information."
+        },
+        {
+          "Page": 1,
+          "Line": 2,
+          "Text": "I've been practicing the art of procrastination to such a degree that it's no longer a procrastination, it's just my way of life."
+        },
+        {
+          "Page": 1,
+          "Line": 3,
+          "Text": "The only way to do great work is to love what you do."
+        }
+      ]
+    },
+    {
+      "Title": "Fahrenheit 451",
+      "ISBN": "9780000528533",
+      "Content": [
+        {
+          "Page": 1,
+          "Line": 1,
+          "Text": "With his symbolic helmet numbered 451 on his stolid head,"
+        },
+        {
+          "Page": 1,
+          "Line": 2,
+          "Text": "and his eyes all orange flame with the though of what came next,"
+        },
+        {
+          "Page": 1,
+          "Line": 3,
+          "Text": "he flicked the igniter and the house jumped up in a gorging fire that burned the evening sky red and yellow and black."
+        },
+      ]
+    }
+  ];
+  
     
 /** Example output object */
 const twentyLeaguesOut = {
@@ -80,6 +150,59 @@ const twentyLeaguesOut = {
             "ISBN": "9780000528531",
             "Page": 31,
             "Line": 9
+        }
+    ]
+}
+
+const extraBooksOutTermProcrastination = {
+    "SearchTerm": "procrastination",
+    "Results": [
+        {
+            "ISBN": "9780000528532",
+            "Page": 1,
+            "Line": 2
+        }
+    ]
+}
+
+const extraBooksOutTermWith = {
+    "SearchTerm": "With",
+    "Results": [
+        {
+            "ISBN": "9780000528533",
+            "Page": 1,
+            "Line": 1
+        },
+    ]
+}
+
+const extraBooksOutTermFranchise = {
+    "SearchTerm": "franchise",
+    "Results": []
+}
+
+const extraBooksOutTermThe = {
+    "SearchTerm": "the",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 9
+        },
+        {
+            "ISBN": "9780000528532",
+            "Page": 1,
+            "Line": 2,
+        },
+        {
+            "ISBN": "9780000528533",
+            "Page": 1,
+            "Line": 2,
+        },
+        {
+            "ISBN": "9780000528533",
+            "Page": 1,
+            "Line": 3,
         }
     ]
 }
@@ -119,3 +242,45 @@ if (test2result.Results.length == 1) {
     console.log("Expected:", twentyLeaguesOut.Results.length);
     console.log("Received:", test2result.Results.length);
 }
+
+/** Positive tests: tests that return a match. */
+const test3result = findSearchTermInBooks("the", extraBooksIn); 
+if (JSON.stringify(extraBooksOutTermThe) === JSON.stringify(test3result)) {
+    console.log("PASS: Test 3");
+} else {
+    console.log("FAIL: Test 3");
+    console.log("Expected:", extraBooksOutTermThe);
+    console.log("Received:", test3result);
+}
+
+/** Negative tests: tests that do not return any matches. */
+const test4result = findSearchTermInBooks("franchise", extraBooksIn); 
+if (JSON.stringify(extraBooksOutTermFranchise) === JSON.stringify(test4result)) {
+    console.log("PASS: Test 4");
+} else {
+    console.log("FAIL: Test 4");
+    console.log("Expected:", extraBooksOutTermFranchise);
+    console.log("Received:", test4result);
+}
+
+// Case-sensitive tests: tests that match (for example) on “The” but not on “the”.
+const test5result = findSearchTermInBooks("With", extraBooksIn); 
+if (JSON.stringify(extraBooksOutTermWith) === JSON.stringify(test5result)) {
+    console.log("PASS: Test 5");
+} else {
+    console.log("FAIL: Test 5");
+    console.log("Expected:", extraBooksOutTermWith);
+    console.log("Received:", test5result);
+}
+
+
+/** This test confirms that a line that contains the serach term twice should only show up in the results once. */
+const test6result = findSearchTermInBooks("procrastination", extraBooksIn); 
+if (JSON.stringify(extraBooksOutTermProcrastination) === JSON.stringify(test6result)) {
+    console.log("PASS: Test 6");
+} else {
+    console.log("FAIL: Test 6");
+    console.log("Expected:", extraBooksOutTermProcrastination);
+    console.log("Received:", test6result);
+}
+
